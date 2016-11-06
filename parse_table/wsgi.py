7fc5c5c6d8f80331b72e72071f31ebfe3d8dc627
Semenyshyn <simen.sims@gmail.com>
2016-11-06 20:28:58 +0200

    1.1.0

--- parse_table/wsgi.py
+++ parse_table/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "<project>.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "parse_table.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
